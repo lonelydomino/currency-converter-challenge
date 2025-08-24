a140c4eb92a54312d1e58b4477e83cd5c09dc217
lonelydomino <lonelydomino@icloud.com>
2025-08-24 00:52:08 -0700

    Fix currency converter logic and improve input validation

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import { useState, useEffect } from "react";
 
 export default function App() {
   const [select1, setSelect1] = useState("USD");
-  const [select2, setSelect2] = useState("USD");
-  const [numInput, setNumInput] = useState('')
+  const [select2, setSelect2] = useState("EUR");
+  const [numInput, setNumInput] = useState("");
+  const [conversion, setConversion] = useState(0);
   useEffect(() => {
-
-  }, [])
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${numInput}&from=${select1}&to=${select2}`
+      );
+      const data = await res.json();
+      const rate = data.rates[select2];
+      setConversion(rate);
+    };
+    if (numInput !== 0 && numInput !== "" && select1 !== select2) fetchData();
+  }, [numInput, select1, select2]);
 
   return (
     <div>
-      <TextInput numInput={numInput} setNumInput={setNumInput}/>
-      <Select setSelect={setSelect1} select={select1}/>
-      <Select setSelect={setSelect2} select={select2}/>
-      <Output />
+      <TextInput numInput={numInput} setNumInput={setNumInput} />
+      <Select setSelect={setSelect1} select={select1} />
+      <Select setSelect={setSelect2} select={select2} />
+      <Output conversion={conversion} select2={select2} />
     </div>
   );
 }
 
-const Select = ({setSelect, select}) => {
+const Select = ({ setSelect, select }) => {
   return (
     <select onChange={(e) => setSelect(e.target.value)} value={select}>
       <option value="USD">USD</option>
@@ -31,10 +40,16 @@ const Select = ({setSelect, select}) => {
   );
 };
 
-const TextInput = ({numInput, setNumInput}) => {
-  return <input type="text" value={numInput} onChange={(e) => setNumInput(Number(e.target.value))}/>
-}
+const TextInput = ({ numInput, setNumInput }) => {
+  return (
+    <input
+      type="text"
+      value={numInput}
+      onChange={(e) => setNumInput(Number(e.target.value))}
+    />
+  );
+};
 
-const Output = () => {
-  return <p>OUTPUT</p>
-}
+const Output = ({ conversion, select2 }) => {
+  return <p>{conversion} {select2} </p>;
+};
